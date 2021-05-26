28e058a91a63ab3ac46534d706771c5ffafc8d27
Santiago Alvarez Barbosa <56238062+SantiagoAlvarezb@users.noreply.github.com>
2021-05-26 10:27:46 -0500

    Última Revisión
    Se documentó el código y se agregó phases.m ya que es implementado en el Simplex

--- Barrier.m
+++ Barrier.m
@@ -23,7 +23,6 @@ for i=1:length(r)
 end
 q
 
-
 %Gradiente de q y ecuaciones obtenidas
 delta_q = [];
 for i=1:length(vars)
@@ -40,9 +39,8 @@ eq6 = delta_q(6)
 
 disp("-----Se esta resolviendo el sistema de ecuaciones aplicando el método de Barrera-----")
 %Resuelve el sistema de ecuaciones anteriores  
-%disp("CHECKPOINT 0.0")
 [solx1,solx2,solx3,solx4,solx5,solx6]= solve(eq1==0,eq2==0,eq3==0,eq4==0,eq5==0,eq6==0);
-%disp("CHECKPOINT 0.1")
+
 %Posibles soluciones de las variables al aplicar el metodo de barrera 
 %disp("-----Para x1-----")
 sol_x1 = [];
@@ -85,13 +83,14 @@ for i=1:length(solx6)-1
     sol_x6 = [sol_x6 double(limit(solx6(i)))];
 end
 sol_x6;
-%Ahora hay que mirar la factibilidad!!!!!!
+
+%Ahora hay que mirar la factibilidad
 disp("-----Hemos obtenido soluciones para las variables!-----") 
 disp("-----Ahora se verifica factibilidad con restricciones-----")
 disp("-----(dura aproximadamente 2 minutos)-----")
 
-%disp("CHECKPOINT 0")
-%Para r1
+%Para r1: Miramos todas las posibles combinaciones de los valores sacados
+%de las soluciones a ver cuales son factibles
 real_ones_r1 = [];
 [a,b,c,d,e,f] = ndgrid(sol_x1,sol_x2,sol_x3,sol_x4,sol_x5,sol_x6);
 pairs = [a(:) b(:) c(:) d(:) e(:) f(:)];
@@ -103,150 +102,94 @@ for i=1:length(pairs)
 end
 real_ones_r1 = unique(real_ones_r1,'rows');
 
-%disp("CHECKPOINT 1")
-
-%para r2 
+%Para r2: Con los valores obtenidos en la sección de código anterior se
+%sustituye los valores de x4 y x5 para mirar factibilidad en segunda
+%restricción
 real_ones_r2 = [];
-[p,q,s] = meshgrid(sol_x4, sol_x5,sol_x6);
-pairas = [p(:) q(:) s(:)];
-for i=1:length(pairas)
-    fact1 = subs(r(2),[x4,x5,x6],[pairas(i,1),pairas(i,2),pairas(i,3)]);
+for i=1:length(real_ones_r1)
+    fact1 = subs(r(2),[x4,x5],[real_ones_r1(i,4),real_ones_r1(i,5)]);
     if fact1 <= 0
-        real_ones_r2 = [real_ones_r2; pairas(i,1),pairas(i,2),pairas(i,3)];
+        real_ones_r2 = [real_ones_r2; real_ones_r1(i,4),real_ones_r1(i,5)];
     end
 end
 real_ones_r2 = unique(real_ones_r2,'rows');
 
 %disp("CHECKPOINT 2")
 
-%para r3
+%Para r3: Mismo procedimiento que el anterior 
 real_ones_r3 = [];
-for i=1:4
-    fact3 = subs(r(3),x1,sol_x1(i));
+for i=1:length(real_ones_r1)
+    fact3 = subs(r(3),x1,real_ones_r1(i,1));
     if double(fact3) <= 0
-       real_ones_r3 = [real_ones_r3; sol_x1(i)];
+       real_ones_r3 = [real_ones_r3; real_ones_r1(i,1)];
     end
 end
 real_ones_r3 = unique(real_ones_r3,'rows');
 
-%disp("CHECKPOINT 3")
-
-%para r4
+%Para r4: Mismo procedimiento que el anterior 
 real_ones_r4 = [];
-for i=1:4
-    fact4 = subs(r(4),x2,sol_x2(i));
+for i=1:length(real_ones_r1)
+    fact4 = subs(r(4),x2,real_ones_r1(i,2));
     if double(fact4) <= 0
-        real_ones_r4 = [real_ones_r4; sol_x2(i)];
+        real_ones_r4 = [real_ones_r4; real_ones_r1(i,2)];
     end
 end
 real_ones_r4 = unique(real_ones_r4,'rows');
 
-%disp("CHECKPOINT 4")
-
-
-%para r5
+%Para r5: Mismo procedimiento que el anterior
 real_ones_r5 = [];
-for i=1:4
-    fact5 = subs(r(5),x4,sol_x4(i));
+for i=1:length(real_ones_r1)
+    fact5 = subs(r(5),x4,real_ones_r1(i,4));
     if double(fact5) <= 0
-        real_ones_r5 = [real_ones_r5; sol_x4(i)];
+        real_ones_r5 = [real_ones_r5; real_ones_r1(i,4)];
     end
 end
 real_ones_r5 = unique(real_ones_r5,'rows');
 
-%disp("CHECKPOINT 5")
-
-
-%para r6
+%Para r6: Mismo procedimiento que el anterior
 real_ones_r6 = [];
-for i=1:4
-    fact6 = subs(r(6),x4,sol_x4(i));
+for i=1:length(real_ones_r1)
+    fact6 = subs(r(6),x4,real_ones_r1(i,4));
     if double(fact6) <= 0
-        real_ones_r6 = [real_ones_r6; sol_x4(i)];
+        real_ones_r6 = [real_ones_r6; real_ones_r1(i,4)];
     end
 end
 real_ones_r6 = unique(real_ones_r6,'rows');
 
-%disp("CHECKPOINT 6")
-
-
-%para r7
+%Para r7: Con los valores obtenidos de x3 y x4 que son factibles con las
+%demas restricciones, se prueba factibilidad para la séptima restricción
 real_ones_r7 = [];
-[t,u] = meshgrid(sol_x3, sol_x4);
-paires = [t(:) u(:)];
-%length(pairs)
-for i=1:length(paires)
-    fact7 = subs(r(7),[x3,x4],[paires(i,1),paires(i,2)]);
+for i=1:length(real_ones_r1)
+    fact7 = subs(r(7),[x3,x4],[real_ones_r1(i,3),real_ones_r1(i,4)]);
     if double(fact7) == 0
-        real_ones_r7 = [real_ones_r7; paires(i,1),paires(i,2)];
+        real_ones_r7 = [real_ones_r7; real_ones_r1(i,3),real_ones_r1(i,4)];
     end
 end
 real_ones_r7 = unique(real_ones_r7,'rows');
 
-%disp("CHECKPOINT 7")
-
-%Con los valores FIJOS encontrados para ciertas variables volvemos a correr la sección anterior para obtener los valores FIJOS de las otras variables  
-
-%Para r1
+%Para r1 2.0: Volvemos a sustituir todos los valores obtenidos para asegurarnos
+%de que se cumpla factibilidad de la primera restricción
 ror1 = [];
 for i=1:length(real_ones_r2)
-    fact1 = subs(r(1),[x1,x2,x3,x4,x5,x6],[real_ones_r3(1),real_ones_r4(1),real_ones_r7(1,1),real_ones_r7(1,2),real_ones_r2(i,2),real_ones_r2(i,3)]);
+    fact1 = subs(r(1),[x1,x2,x3,x4,x5,x6],[real_ones_r3(1),real_ones_r4(1),real_ones_r7(1,1),real_ones_r7(1,2),real_ones_r2(1,2),real_ones_r1(1,6)]);
     if double(fact1) <= 0
-        ror1 = [ror1; real_ones_r3(1),real_ones_r4(1),real_ones_r7(1,1),real_ones_r7(1,2),real_ones_r2(i,2),real_ones_r2(i,3)];
+        ror1 = [ror1; real_ones_r3(1),real_ones_r4(1),real_ones_r7(1,1),real_ones_r7(1,2),real_ones_r2(1,2),real_ones_r1(1,6)];
     end
 end
 ror1 = unique(ror1,'rows');
 
-%disp("CHECKPOINT 8")
-
-
-%para r2 NO TOCAR
-ror2 = [];
-for i=1:3
-    fact2 = subs(r(2),[x4,x5,x6],[real_ones_r7(1,2),ror1(i,5),ror1(i,6)]);
-    if double(fact2) <= 0
-        ror2 = [ror2; real_ones_r7(1,2),ror1(i,5),ror1(i,6)];
-    end
-end
-ror2 = unique(ror2,'rows');
-
-%disp("CHECKPOINT 9")
-
-
-%Ya tenemos los valores para todas las variables sin embargo, para unas
-%variables hay multiples valores que son factibles
-%Entonces sistituimos los valores en la FO y encontramos los valores que
-%produzca el max valor 
-f= 0.25*x1 + 0.3*x2 + 0.35*x3 + 0.85*x4 + 0.75*x5 + 0.9*x6;
-sol1 = subs(f, [x1,x2,x3,x4,x5,x6], [real_ones_r3(1),real_ones_r4(1),real_ones_r7(1,1),real_ones_r7(1,2),ror2(1,2),ror2(1,3)]); 
-sol2 = subs(f, [x1,x2,x3,x4,x5,x6], [real_ones_r3(1),real_ones_r4(1),real_ones_r7(1,1),real_ones_r7(1,2),ror2(2,2),ror2(2,3)]);
-
+%Ya tenemos los valores para todas las variables 
 %La Solución
 disp("-----Tenemos la siguiente solución al problema no lineal!-----")
-if sol1 > sol2
     disp("Para x1")
-    solucion_r1 = real_ones_r3(1)
+    solucion_r1 = ror1(1)
     disp("Para x2")
-    solucion_r2 = real_ones_r4(1)
+    solucion_r2 = ror1(2)
     disp("Para x3")
-    solucion_r3 = real_ones_r7(1,1)
+    solucion_r3 = ror1(3)
     disp("Para x4")
-    solucion_r4 = real_ones_r7(1,2)
+    solucion_r4 = ror1(4)
     disp("Para x5")
-    solucion_r5 = ror2(1,2)
+    solucion_r5 = ror1(5)
     disp("Para x6")
-    solucion_r6 = 3000000 - (solucion_r1 + solucion_r2 + solucion_r3 + solucion_r4 + solucion_r5) 
-else
-    disp("Para x1")
-    solucion_r1 = real_ones_r3(1)
-    disp("Para x2")
-    solucion_r2 = real_ones_r4(1)
-    disp("Para x3")
-    solucion_r3 = real_ones_r7(1,1)
-    disp("Para x4")
-    solucion_r4 = real_ones_r7(1,2)
-    disp("Para x5")
-    solucion_r5 = ror2(2,2) 
-    disp("Para x6")
-    solucion_r6 = 3000000 - (solucion_r1 + solucion_r2 + solucion_r3 + solucion_r4 + solucion_r5) 
-end    
+    solucion_r6 = 3000000 - (solucion_r1 + solucion_r2 + solucion_r3 + solucion_r4 + solucion_r5) %Nos aseguramos en que no se vaya por encima del presupuesto    
--- phases.m
+++ phases.m
@@ -1,54 +1,38 @@
-%MODO SIMPLEX SILENCE PARA EL PROYECTO  
-% Por favor ingrese las siguientes lineas en la terminal de Matlab y después llame
-% la funcion: Simplex_silence(cost_fs, A_fs,b,h)
-    %cost_fs = [0.25 0.3 0.3 0.85 0.75 0.9 0 0 0 0 0 0 0];
-    %A_fs = [
-    %         0.06 0.14 0.3 0.6 0.8 0.9 -1 0 0 0 0 0 0
-    %         0 0 0 1 1 0 0 0 1 0 0 0 0;
-    %         1 0 0 0 0 0 0 0 0 1 0 0 0;
-    %         0 1 0 0 0 0 0 0 0 0 -1 0 0;
-    %         0 0 0 1 0 0 0 0 0 0 0 -1 0;
-    %         0 0 0 1 0 0 0 0 0 0 0 0 1];  
-    %b = [2750000;600000;1500000;500000;90000;290000];
-    %h = 1;
-    
-function [M] = Simplex_silence(cost_fs, A_fs,b,h)
-    % h == 1: Para problemas de MAXIMIZACIÓN
-    % h == 0: Para problemas de MINIMIZACIÓN
-    disp("SHHHHH! Silent Simplex is about to start")
-    pause(1)
-    disp(".")
-    pause(1)
-    disp(".")
-    pause(1)
-    disp(".")
-    pause(1)
-     
-    %Primero se ejecuta el código sobre el método de las dos fases para
-    %encontrar un punto inicial 
-    [Ib,In] = phases(cost_fs, A_fs, b); 
-    Ib;
-    In;
+% Método de las fases que hace hace previo al SIMPLEX para encontrar los
+% indices con los cuales debemos empezar 
+function [Ib,In] = phases(cost_fn, A_fn, b)
 
-    condicion = 1; 
+    %Se transforma el problema a formato estándar
+    artificial = eye(size(A_fn,1));
+    A_fs = [A_fn artificial];
+    cost_artificial_fn = ones([1,size(A_fn,1)]);
+    cost_zero = zeros([1,size(A_fn,2)]);
+    cost_fs = [cost_zero cost_artificial_fn];
+
+    Ib = [];
+    In = [];
+    for i=1:length(cost_fs)
+       if cost_fs(i) ~= 0
+           Ib = [Ib i];
+       else
+           In = [In i];
+       end
+    end
+
     
-    %Se entra al proceso iterativo del SIMPLEX
+    cost_red = ones(size((cost_fs(:,In)))) .* -1; 
+    condicion = 1;
+    
     while condicion == 1
+        %Paso 1: Encontrar información general
         condicion = 0;
-        
-        %Paso 1: Se obtiene las bases, Xb y el valor de Z0
         B = (A_fs(:,Ib));
         B_inv = inv(B);
         Xb = B_inv*b;
-                
         Cb = (cost_fs(:,Ib));
-        Z0 = Cb*Xb;
-        
-        if h == 1
-            Z0 = -1*Z0;
-        end
-        
-        % Paso 2: Costos Reducidos 
+
+
+        %Paso 2: Costos reducidos
         Cj = (cost_fs(:,In));
         N = (A_fs(:,In));
         Zj = Cb*B_inv*N;
@@ -61,6 +45,7 @@ function [M] = Simplex_silence(cost_fs, A_fs,b,h)
            end
         end
         
+        %Cirterio de parada del algoritmo 
         if condicion == 0
             X=[];
             indxb=1;
@@ -72,9 +57,16 @@ function [M] = Simplex_silence(cost_fs, A_fs,b,h)
                     X=[X 0];
                 end
             end
+               for i=length(X):-1:1
+                   if X(i) ~= 0 && (length(X)-length(artificial)) < i
+                       msg = "Original problem has no feasable solution. Therefore Simplex won't work"
+                       f = msgbox(msg)
+                       error(msg)
+                   end 
+               end
                break
         end
-        
+
         lowest_num = min(cost_red);
         for i=1:length(cost_red)
            if cost_red(i) == lowest_num && cost_red(i) < 0
@@ -83,21 +75,14 @@ function [M] = Simplex_silence(cost_fs, A_fs,b,h)
            end
         end
 
-        %Paso 3: Criterio de la razón mínima
+        %Paso 3: Criterio de la Razón mínimaa
         yk = B_inv*A_fs(:,variable_entrar);
         min_vals = transpose(Xb./yk);
-        
         new_min_vals = [];
         for i=1:length(min_vals)
-            if min_vals(i) ~= inf && min_vals(i) > 0
+            if min_vals(i) ~= inf && min_vals(i) > 0 
                new_min_vals = [new_min_vals min_vals(i)];
-            end
-        end
-        
-        if length(new_min_vals) == 0
-            msg = "Original problem has an unbounded optimal solution!"
-            f = msgbox(msg)
-            error(msg)
+           end
         end
 
         lowest_num_criterio_min = min(new_min_vals);
@@ -107,7 +92,7 @@ function [M] = Simplex_silence(cost_fs, A_fs,b,h)
                break
            end
         end
-        
+
         indice_salir = find(Ib==variable_salir);
         indice_entrar = find(In==variable_entrar);
 
@@ -127,10 +112,8 @@ function [M] = Simplex_silence(cost_fs, A_fs,b,h)
         end
     end
     
-    disp("-- Optimal Solution --")
-    Xb;
-    J = transpose(X);
-    J(6,1) = 3000000 - (J(1,1) + J(2,1) + J(3,1) + J(4,1) + J(5,1));
-    M = J;
-    Z0;
+    for i=1:length(cost_artificial_fn)
+        In(end) = [];
+    end 
 end
+
